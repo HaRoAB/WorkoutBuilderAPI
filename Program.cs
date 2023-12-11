55ee06de95798a9123f5c9885dcc6982ab97c09c
Hanna <hannamymadsen89@gmail.com>
2023-12-11 10:49:02 +0100

    testa api

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using WorkoutBuilderAPI.Application.Infrastructure;
 using WorkoutBuilderAPI.Application.Interfaces;
 using WorkoutBuilderAPI.Application.Services;
 using WorkoutBuilderAPI.DataMigration;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,15 +15,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IWorkoutService, WorkoutService>();
 //builder.Services.AddScoped<IWorkoutRepository, JsonRepository>();
 
-
 // DataMigration.MigrateDataToMongoDB();
-//builder.Services.AddScoped<IWorkoutRepository>(_ =>
+// builder.Services.AddScoped<IWorkoutRepository>(_ =>
 // {
 //     var connectionString = "mongodb+srv://Hannsis:lollipop123@cluster0.wvu1dqq.mongodb.net/";
 //     var mongoClient = new MongoClient(connectionString);
-    
+
 //     return new MongoDbRepo(mongoClient, "all_your_database_are_belong_to_us");
 // });
+
+//string connectionString = Environment.GetEnvironmentVariable("MONOGODB_CONNECTION_STRING");
+string connectionString = "mongodb+srv://Hannsis:lollipop123@cluster0.wvu1dqq.mongodb.net/";
+MongoClient client = new(connectionString);
+
+IMongoDatabase database = client.GetDatabase(Environment.GetEnvironmentVariable("all_your_database_are_belong_to_us"));
+//IMongoDatabase database = client.GetDatabase(Environment.GetEnvironmentVariable("MONGO_DATABASE_NAME"));
 
 builder.Services.AddScoped<IWorkoutRepository, MongoDbRepo>();
 
